a47ca9259c16008397ea69f5b15419e58133c76a
Katye Russell <katye.russell@gmail.com>
2015-04-14 00:27:13 -0500

    Stuck on how to add next_item method (test_02a).

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,8 +1,28 @@
 class LinkedListItem
   include Comparable
   attr_reader :payload
+  attr_writer :next_item
 
   def initialize(data)
     @payload = data
   end
+
+  def intialize(next_item=nil)
+    @next_item = next_item
+  end
+
 end
+
+# test_00a_triple_equals & test_01_item_stores_payload
+lli = LinkedListItem.new("foo")
+
+# test_01b_item_stores_payload
+lli = LinkedListItem.new("bar")
+
+
+# test_01_item_stores_payload
+lli1 = LinkedListItem.new("foo")
+lli2 = LinkedListItem.new("bar")
+
+lli1.next_item = lli2
+
