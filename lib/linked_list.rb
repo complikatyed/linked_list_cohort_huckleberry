0d847fec9028c38f814ee3c4f64e7fb0b5c06cbc
Katye Russell <katye.russell@gmail.com>
2015-04-14 15:54:28 -0500

    Really just adding 'what if' notes.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,35 @@
 class LinkedList
 
+  # Wondering if the whole LinkedListItem class definition needs to go right here?
+  # That way when a new item is created, it's coming from inside here...
+  #
+  # Because shouldn't all of the things that are getting pushed up into the
+  # new linked_list BE linked_list_items?
+  # 
+  # So wouldn't the "push" method actually involve creating a new Linked_List_Item?
+  #
+  # Also, while I like the (*data) option with differing variables, I'm not sure that's right...
 
-  
+
+  attr_accessor :payload, :first_item, :index, :value
+
+  def initialize(*data)
+    @payload = data
+  end
+
+  def push(value)
+    @payload = value
+  end
+
+#  def index(value)
+    #@first_item = index(value)
+  #end
+
+  def get(first_item)
+    @first_item = index(value)
+  end
 end
+
 
 
 
