4803acbb129499c3ddd0e99eb75ab15decd416a6
Katye Russell <katye.russell@gmail.com>
2015-04-15 10:12:31 -0500

    Taking out the LinkedListItem code.  Still stuck on index problem.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,34 +1,3 @@
-class LinkedListItem
-  include Comparable
-    attr_reader :payload
-    attr_reader :next_item
-
-#   def initialize(data)
-     #@payload = data
-   #end
-
-   def last?
-     next_item.nil?
-   end
-
-   def next_item=(lli)
-     raise ArgumentError, "can't next_item to self!" if self == lli
-     @next_item = lli
-   end
-
-   def <=>(other)
-     if self.payload.class == other.payload.class
-       self.payload <=> other.payload
-     else
-       self.payload.class.to_s <=> other.payload.class.to_s
-     end
-   end
-
-   def ===(other)
-     self.object_id === other.object_id
-   end
-end
-
 class LinkedList
   attr_accessor :payload, :first_item, :item, :ll, :list, :value, :index, :push, :new, :get, :next_item
 
