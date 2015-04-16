9f1d66b8e1cc44a70eae5ca08bf6697e893326ec
Katye Russell <katye.russell@gmail.com>
2015-04-16 10:21:11 -0500

    Ass tests passing.

--- ll.rb
+++ ll.rb
@@ -6,26 +6,18 @@ payloads = ARGV # ARGV holds the command line arguments that were passed in
 # Now, build a LinkedList
 ll = LinkedList.new
 
-# And, populate it
 
-#TODO: Push all of the items from the array into the LinkedList
-#By pushing them in, turns them into LinkedListItems
-
-unless payloads.empty?
-  ll.push payloads(0)
+payloads.each do |payload|
+  ll.push payload
 end
 
-ll.push(ARGV)
+print "* -> "
 
-# Then, print it out
-# POSSIBLE CHOICES:  print ll
-# POSSIBLE CHOICES:  loop through the ll values
-# MIGHT WANT TO: check for actual values in ll (and output "* => nil")
-if ll.first_item
-  print "* -> #{ll.get(0).inspect} -> nil"
-else
-  print "* -> nil"
+ll.size.times do |i|
+  print "#{ll.get(i).inspect} -> "
 end
+
+print "nil"
 
 
 
