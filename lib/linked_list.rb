7fef3345336265162febae5f75a4771252b602e1
Katye Russell <katye.russell@gmail.com>
2015-04-15 20:11:52 -0500

    Refactors with Erin - now working on _test_09a_

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,27 +1,57 @@
+require_relative 'linked_list_item'
+  
 class LinkedList
+  attr_accessor :first_item, :size, :last_item
 
-  def get(nth_item)
-    if nth_item == 0
+  def initialize
+    @size = 0
+    @first_item = nil
+    @last_item = nil
+  end
+
+  def push(payload)
+    if @first_item.nil?
+      @first_item = LinkedListItem.new(payload)
+      @last_item = @first_item
+      @size += 1
+    else
+      @last_item.next_item = LinkedListItem.new(payload)
+      @last_item = @last_item.next_item
+      @size += 1
+    end
+  end
+
+  def get(index)
+    # index number can't be less than zero, so IndexError gets raised
+    raise IndexError, "Sorry, Charlie." if index < 0
+
+    # if the index number given is not in the range of the index...
+    raise IndexError, "Nope." if !(0..@size).include?(index)
+
+    if index == 0
       @first_item.payload
     else
-      count = 0
-      current_node = @first_item
-      while count < nth_item
-        current_node = current_node.next_item
-        count = count + 1
+      current_item = @first_item
+      counter = index
+
+      counter.times do # index number of times get the next_item.payload
+        # get the next item -- starting at first_item
+       current_item = current_item.next_item
       end
-      current_node.payload
+
+    current_item.payload
     end
   end
 
-  def push(item)
-    if @first_item.nil?
-      @first_item = LinkedListItem.new(item)
-      @last_item = @first_item
+  def last
+    if @last_item.nil?
+      @last_item
     else
-      @last_item.next_item = LinkedListItem.new(item)
-      @last_item = @last_item.next_item
+      @last_item.payload
     end
   end
+  
 end
+
+
 
