732ae08323d1f108988ecb991a6f969bdc5c27e5
Katye Russell <katye.russell@gmail.com>
2015-04-20 09:28:47 -0500

    Rearranged code slightly for readability. Passing through _13f_

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,9 +3,9 @@ require_relative 'linked_list_item'
 
 class LinkedList
   attr_accessor :payload, :next_item, :size, :ll
+
   def initialize(*payload)
     @size = 0
-
     payload.each do |payload|
       push(payload)
     end
@@ -22,29 +22,9 @@ class LinkedList
     @last_item = new_item
   end
 
-
-  def delete(index)
-    raise IndexError, "Nope." if !(0..@size).include?(index)
-
-    if index == 0
-      @first_item = @first_item.next_item  # This is passing test_13c_ (remove first item)
-    else
-      current_node = @first_item
-      (index -1).times do
-        current_node = current_node.next_item
-      end
-      current_node.next_item = current_node.next_item.next_item
-    end
-    @size -= 1  # And passing test_13a (delete changes size)
-  end
-
   def get(index)
-    # index number can't be less than zero, so IndexError gets raised
     raise IndexError, "Sorry, Charlie." if index < 0
-
-    # if the index number given is not in the range of the index...
     raise IndexError, "Nope." if !(0..@size).include?(index)
-
     if index == 0
       @first_item.payload
     else
@@ -57,22 +37,10 @@ class LinkedList
     end
   end
 
-  def index(value)
-    puts value
-  end
-
   def last
     unless @last_item.nil?
       @last_item.payload
     end
-  end
-
-  def [](index)
-    get(index)
-  end
-
-  def []=(index, object)
-    get(index).replace object
   end
 
   def to_s
@@ -88,6 +56,33 @@ class LinkedList
     str
   end
 
+  def delete(index)
+    raise IndexError, "Nope." if !(0..@size).include?(index)
+
+    if index == 0
+      @first_item = @first_item.next_item
+    else
+      current_node = @first_item
+      (index -1).times do
+        current_node = current_node.next_item
+      end
+      current_node.next_item = current_node.next_item.next_item
+    end
+    @size -= 1
+  end
+
+  def [](index)
+    get(index)
+  end
+
+  def []=(index, value)
+    get(index).replace value
+  end
+
+  def index(value)
+    puts value  # Checking that the value IS being seen (It is).
+
+  end
 end
 
 
